9aa7ca8df5e56706176dfba975b9589ac2ae4b7e
Seal <polloconuncsne@gmail.com>
2026-08-26 11:24:39 +0200

    Changed the wallpaper picker UI

--- colors/scripts/matugen_colors.sh
+++ colors/scripts/matugen_colors.sh
@@ -6,35 +6,43 @@ set -e
 WALLPAPER_DIR="${HOME}/.config/colors/wallpapers"
 ACTIVE_DIR="${WALLPAPER_DIR}/active"
 ACTIVE_WALLPAPER="${ACTIVE_DIR}/active.jpg"
+ROFI_THEME="$HOME/.config/rofi/wallpaper_picker_config.rasi"
 
-# Select wallpaper
-WALLPAPER_NAME=$(
-    find "$WALLPAPER_DIR" \
-        -maxdepth 1 \
-        -type f \
-        -exec basename {} \; | \
-    rofi -dmenu -p "Enter wallpaper name:"
+WALLPAPER=$(
+	find "$WALLPAPER_DIR" -maxdepth 1 -type f | while read -r img; do
+    [[ "$img" =~ \.(jpg|jpeg|png|webp|JPG|JPEG|PNG|WEBP)$ ]] || continue
+
+
+    REL_PATH="${img#$WALLPAPER_DIR/}"
+	printf "%s\0icon\x1f%s\n" "$REL_PATH" "$img"
+    done | rofi \
+        -dmenu \
+        -i \
+        -show-icons \
+        -theme "$ROFI_THEME" \
+        -p ""
 )
 
-[ -z "$WALLPAPER_NAME" ] && exit 0
 
-WALLPAPER="${WALLPAPER_DIR}/${WALLPAPER_NAME}"
+[ -z "$WALLPAPER" ] && exit 0
 
 echo "Selected wallpaper: $WALLPAPER"
 
+WP_PATH="$WALLPAPER_DIR/$WALLPAPER"
+
 # Save active wallpaper
 mkdir -p "$ACTIVE_DIR"
-cp "$WALLPAPER" "$ACTIVE_WALLPAPER"
+cp "$WP_PATH" "$ACTIVE_WALLPAPER"
 
 # Generate colors with matugen
-matugen image "$WALLPAPER"
+matugen image "$WP_PATH"
 
 # Smooth transition
 niri msg action do-screen-transition --delay-ms 300
 
 # Restart swaybg
 pkill -x swaybg || true
-nohup swaybg -i "$WALLPAPER" -m fill >/dev/null 2>&1 &
+nohup swaybg -i "$WP_PATH" -m fill >/dev/null 2>&1 &
 
 # Reload swaync
 swaync-client -rs
